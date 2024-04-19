ef45c66ac39d126aa558d59b19fc40a55088368c
usuario <lxin@elpuig.xeill.net>
2024-04-19 21:29:14 +0200

    .

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const menuBase = document.querySelector('.j-menu-base');
+let lastScrollDirection = null;
 
 window.addEventListener('scroll', function() {
-  const scrollY = window.scrollY; // Get the current scroll position
+  const scrollY = window.scrollY;
+  const newScrollDirection = scrollY > lastScrollDirection ? 'down' : 'up';
 
-  if (scrollY > 0) {
-    menuBase.classList.add('scrolled');
-  } else {
-    menuBase.classList.remove('scrolled');
+  if (newScrollDirection === 'down' && !menuBase.classList.contains('hidden')) {
+    menuBase.classList.add('hidden');
+  } else if (newScrollDirection === 'up' && menuBase.classList.contains('hidden')) {
+    menuBase.classList.remove('hidden');
   }
+
+  lastScrollDirection = scrollY;
 });
