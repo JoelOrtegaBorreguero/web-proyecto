b42906eaa285b4ba9d447033f7ce653f5176cfe0
usuario <lxin@elpuig.xeill.net>
2024-05-02 15:42:21 +0200

    a

--- main.js
+++ main.js
@@ -14,16 +14,4 @@ fetch("./footer.html")
     document.querySelector("footer").innerHTML = data;
   });
 
-const menu = document.querySelector('.j-menu-base');
-let prevScrollPos = window.pageYOffset;
-  
-window.onscroll = function() {
-  const currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    menu.classList.remove('hidden'); // Show menu on scroll up
-  } else {
-    menu.classList.add('hidden'); // Hide menu on scroll down
-  }
-  prevScrollPos = currentScrollPos;
-};
   
