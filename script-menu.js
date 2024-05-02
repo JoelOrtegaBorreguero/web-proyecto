b576e63fe3dad7fe231d06c35249bc9cd23019dd
usuario <lxin@elpuig.xeill.net>
2024-05-02 15:50:36 +0200

    a

--- script-menu.js
+++ script-menu.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('.j-menu-contenedor');
+const menu = document.querySelector('.j-menu-base');
 let prevScrollPos = window.pageYOffset;
 
 window.onscroll = function() {
